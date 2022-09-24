75f80c22aeabd5b6660bce13c809eaec52f8b379
kuznetsovmatweyushka <kuznetsovmatweyushka@yandex.ru>
2022-09-24 11:21:36 +0300

    sem 5

---
+++ Seminars/Sem_5/gui.py
@@ -0,0 +1,9 @@
+
+def get_value():
+    return int(input('Значение = '))
+    
+def print_result(data, title):
+    print(f'{title} {data}')
+
+def action():
+    return input('Какое действие? ')
---
+++ Seminars/Sem_5/math_f.py
@@ -0,0 +1,11 @@
+def math_sum(x, y):
+    return x + y
+
+def math_mult(x, y):
+    return x * y
+
+def math_sub(x, y):
+    return x - y
+
+def math_div(x, y):
+    return x / y
